0e214cac826bb7f56a22dbaaad06211dfecb95f8
Matěj <mckillem@tuta.io>
2025-04-17 09:24:16 +0200

    úpravy zobrazení hlavičky

--- .docker/build/database/01_create.sql
+++ .docker/build/database/01_create.sql
@@ -23,13 +23,13 @@ CREATE TABLE IF NOT EXISTS page (
                                       UNIQUE(`url`)
 );
 
-INSERT INTO `page` (`title`, `content`, `url`, `description`, `controller`) VALUES
-                                                                                   ('Úvod', '<p>Vítejte na mé stránce.</p>\r\n\r\n<p>Najdete na ní ukázku mé tvorby a nabídku služeb spolu s kontaktem.</p>', 'uvod', 'Úvodní stránka', NULL),
-                                                                                   ('Přihlášení', NULL, 'prihlaseni', 'Přihlášení do uživatelského účtu.', 'Authenticator\\Controllers\\Login'),
-                                                                                   ('Stránka nebyla nalezena', '<p>Litujeme, ale požadovaná stránka nebyla nalezena. Zkontrolujte prosím URL adresu.</p>', 'chyba', 'Stránka nebyla nalezena.', 'Controllers\\Error'),
-                                                                                   ('Kontaktní formulář', NULL, 'kontakt', 'Kontaktní formulář', 'Controllers\\Contact'),
-                                                                                   ('Služby', '<p>Tvorba webů převážně backend v PHP.</p>', 'sluzby', 'Služby', 'Controllers\\Service'),
-                                                                                   ('Tvorba', '<a href="https://github.com/mckillem" target="_blank">Odkaz na můj GitHub profil</a>', 'tvorba', 'Tvorba', 'Controllers\\Creation'),
-                                                                                ('Editor', NULL, 'editor', 'Editor stránek', 'Pages\\Controllers\\Editor'),
-                                                                                ('Seznam stránek', NULL, 'seznam-stranek', 'Seznam stránek', 'Pages\\Controllers\\PageList'),
-                                                                                   ('Administrace webu', NULL, 'administrace', 'Administrace webu', 'Authenticator\\Controllers\\Authenticator');
+INSERT INTO `page` (`title`, `content`, `url`, `description`, `controller`)
+VALUES ('Úvod', '<p>Vítejte na mé stránce.</p>\r\n\r\n<p>Najdete na ní ukázku mé tvorby a nabídku služeb spolu s kontaktem.</p>', 'uvod', 'Úvodní stránka', NULL),
+       ('Služby', '<p>Tvorba webů převážně backend v PHP.</p>', 'sluzby', 'Služby', 'Controllers\\Service'),
+       ('Tvorba', '<a href="https://github.com/mckillem" target="_blank">Odkaz na můj GitHub profil</a>', 'tvorba', 'Tvorba', 'Controllers\\Creation'),
+       ('Kontaktní formulář', NULL, 'kontakt', 'Kontaktní formulář', 'Controllers\\Contact'),
+       ('Přihlášení', NULL, 'prihlaseni', 'Přihlášení do uživatelského účtu.', 'Authenticator\\Controllers\\Login'),
+       ('Stránka nebyla nalezena', '<p>Litujeme, ale požadovaná stránka nebyla nalezena. Zkontrolujte prosím URL adresu.</p>', 'chyba', 'Stránka nebyla nalezena.', 'Controllers\\Error'),
+       ('Editor', NULL, 'editor', 'Editor stránek', 'Pages\\Controllers\\Editor'),
+       ('Seznam stránek', NULL, 'seznam-stranek', 'Seznam stránek', 'Pages\\Controllers\\PageList'),
+       ('Administrace webu', NULL, 'administrace', 'Administrace webu', 'Authenticator\\Controllers\\Authenticator');
