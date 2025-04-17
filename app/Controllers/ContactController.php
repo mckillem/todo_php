6febb0198f6d8edc70ae99c2bdabbd6a01cdcc86
Matěj <mckillem@tuta.io>
2025-04-17 09:59:31 +0200

    metoda pro odeslání emailu

--- app/Controllers/ContactController.php
+++ app/Controllers/ContactController.php
@@ -6,6 +6,26 @@ class ContactController extends Controller
 {
 	public function index(): void
 	{
+//		var_dump($_POST ?? 'prazdno');
+		if (isset($_POST))
+			$this->sendEmail($_POST['email'], $_POST['text']);
 		$this->view = 'index';
+	}
+
+	private function sendEmail(string $email, string $text): void
+	{
+		$to      = 'mckillem@tuta.io';
+		$subject = 'Email z webu';
+		$message = $text;
+		$headers = 'From: ' . $email . "\n" .
+			'Reply-To: ' . $email . "\n" .
+			'X-Mailer: PHP/' . phpversion();
+
+		$success = mail($to, $subject, $message, $headers);
+		var_dump($success);
+		if (!$success) {
+			$errorMessage = error_get_last()['message'];
+			var_dump($errorMessage);
+		}
 	}
 }
